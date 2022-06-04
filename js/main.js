2f63092959095367987570855ca01050a738d73a
tai <dinhtai0293@gmail.com>
2022-06-04 10:43:27 +0700

    up

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@ var x = document.querySelector('.header_content .nav')
 var y = document.querySelector('.header_content .fa-list')
 var z = document.querySelector('.header_content .fa-xmark')
 var btn = document.querySelector('.header_content .btn')
-btn.onclick = function(){
-    if(x.style.display == 'flex'){
+btn.onclick = function () {
+    if (x.style.display == 'flex') {
         x.style.display = 'none'
         y.style.display = 'block'
         z.style.display = 'none'
-    }else{
+    } else {
         x.style.display = 'flex'
         y.style.display = 'none'
         z.style.display = 'block'
@@ -22,7 +22,7 @@ var text3 = document.querySelector('.banner_text .list .text3')
 var col1 = document.querySelector('.banner_text .row .col1')
 var col2 = document.querySelector('.banner_text .row .col2')
 var col3 = document.querySelector('.banner_text .row .col3')
-text1.onclick = function(){
+text1.onclick = function () {
     col1.style.display = 'block'
     col2.style.display = 'none'
     col3.style.display = 'none'
@@ -30,7 +30,7 @@ text1.onclick = function(){
     text2.classList.remove('active')
     text3.classList.remove('active')
 }
-text2.onclick = function(){
+text2.onclick = function () {
     col1.style.display = 'none'
     col2.style.display = 'block'
     col3.style.display = 'none'
@@ -38,7 +38,7 @@ text2.onclick = function(){
     text2.classList.add('active')
     text3.classList.remove('active')
 }
-text3.onclick = function(){
+text3.onclick = function () {
     col1.style.display = 'none'
     col2.style.display = 'none'
     col3.style.display = 'block'
@@ -46,3 +46,4 @@ text3.onclick = function(){
     text2.classList.remove('active')
     text3.classList.add('active')
 }
+
